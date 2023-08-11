08f161077e8964c6e9f41a3f9846c61c29af6593
Nikki <niklas.stooss@gmail.com>
2023-08-11 22:10:25 +0200

    started with the makefile and added some functions

---
+++ Makefile
@@ -0,0 +1,35 @@
+SRC_PATH		= 		./Libc_functions/
+SRC_NAME		=		ft_isalpha.c \
+						ft_isdigit.c \
+						ft_isalnum.c \
+						ft_isascii.c \
+						ft_isprint.c \
+						ft_strlen.c \
+						ft_memset.c \
+						ft_bzero.c \
+						ft_memcpy.c \
+						ft_memmove.c \
+						ft_strlcpy.c \
+						ft_strlcat.c \
+						ft_toupper.c \
+						ft_tolower.c \
+						ft_strchr.c \
+						ft_strrchr.c \
+						ft_strncmp.c \
+						ft_memchr.c \
+						ft_memcmp.c \
+						ft_strnstr.c \
+						ft_atoi.c \
+						ft_calloc.c \
+						ft_strdup.c \
+
+OBJ_NAME		=		$(SRC_NAME:.c=.o)
+SRC_NAME		=		$(addprefix $(SRC_PATH), $(SRC_NAME))
+HEADER			=		./includes/
+CFLAGS			=		-Wall -Wextra -Werror
+NAME			=		libft.a
+
+all:	$(NAME)
+
+$(NAME):
+	
--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: nikki <nikki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 13:45:43 by nikki             #+#    #+#             */
-/*   Updated: 2023/08/10 08:52:47 by nikki            ###   ########.fr       */
+/*   Updated: 2023/08/11 22:08:50 by nikki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,14 @@ size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
 int     ft_toupper(int c);
 int     ft_tolower(int c);
-
+char    *ft_strchr(const char *s, int c);
+char    *ft_strrchr(const char *s, int c);
+int     ft_strncmp(const char *s1, const char *s2, size_t n);
+void    *ft_memchr(const void *s, int c, size_t n);
+int     ft_memcmp(const void *s1, const void *s2, size_t n);
+char    *ft_strnstr(const char *haystack, const char *needle, size_t n);
+int     ft_atoi(const char *nptr);
+void    *ft_calloc(size_t nmemb, size_t size);
+char    *ft_strdup(const char *s);
 
 #endif
