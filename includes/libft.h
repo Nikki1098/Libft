120c451ed2ac4e6bece0aa2469b5348cdaa5148c
Nikki <niklas.stooss@gmail.com>
2023-08-11 23:29:03 +0200

    some more functions

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: nikki <nikki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 13:45:43 by nikki             #+#    #+#             */
-/*   Updated: 2023/08/11 22:08:50 by nikki            ###   ########.fr       */
+/*   Updated: 2023/08/11 22:12:42 by nikki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ int     ft_strncmp(const char *s1, const char *s2, size_t n);
 void    *ft_memchr(const void *s, int c, size_t n);
 int     ft_memcmp(const void *s1, const void *s2, size_t n);
 char    *ft_strnstr(const char *haystack, const char *needle, size_t n);
-int     ft_atoi(const char *nptr);
+int     ft_atoi(const char *str);
 void    *ft_calloc(size_t nmemb, size_t size);
 char    *ft_strdup(const char *s);
 
